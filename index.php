f561ce2d7dfd35289f13bd024fbaaea7967be888
Duex <deux02030@gmail.com>
2017-11-09 22:24:36 -0500

    initial project files

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>To Do Application</title>
+	<link href="https://fonts.googleapis.com/css?family=Spectral+SC" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light|Spectral+SC" rel="stylesheet">
+	<link rel="stylesheet" href="main.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	<div class="list">
+		<h1 class="header">To do List</h1>
+		<ul class="items">
+			<li>
+				<span class="item">Do HackerRank lesson</span>
+				<a href="#" class="done-button">Done</a>
+			</li>
+			<li>
+				<span class="item done">Make dinner</span>
+			</li>
+		</ul>
+		<form class="add-item" action="add.php" method="post">
+			<input type="text" name="name" placeholder="add item here" class="input" autocomplete="off" required>
+			<input type="submit" value="Add Item" class="submit">
+		</form>
+	</div>
+	</body>
+	</html>
+
